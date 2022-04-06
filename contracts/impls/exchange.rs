627f0734f5bbe64cba356647a8fe622112374a30
PierreOssun <pierre.giraud@mailfence.com>
2022-04-06 18:16:35 +0200

    Added spaces

--- contracts/impls/exchange.rs
+++ contracts/impls/exchange.rs
@@ -16,21 +16,25 @@ impl<T: ExchangeStorage> Exchange for T {
             .checked_sub(transfered_value)
             .ok_or(ExchangeError::SubUnderflow)?;
         let tokens_bought = self.price(transfered_value, input_reserve, token_reserve)?;
+
         UsdTokenRef::transfer(
             &self.get().usd_token,
             Self::env().caller(),
             tokens_bought,
             Vec::<u8>::new(),
         )?;
+
         Ok(tokens_bought)
     }
 
     default fn token_to_native(&mut self, token_amount: u128) -> Result<u128, ExchangeError> {
         let token_reserve = UsdTokenRef::balance_of(&self.get().usd_token, Self::env().account_id());
         let native_bought = self.price(token_amount, token_reserve, Self::env().balance())?;
+
         if Self::env().transfer(Self::env().caller(), token_amount).is_err() {
             return Err(ExchangeError::NativeTransferFailed)
         }
+
         Ok(native_bought)
     }
 
@@ -49,17 +53,20 @@ impl<T: ExchangeStorage> Exchange for T {
             .ok_or(ExchangeError::MulOverflow)?
             .checked_add(input_amount_with_fee)
             .ok_or(ExchangeError::AddOverflow)?;
+
         numerator.checked_div(denominator).ok_or(ExchangeError::DivByZero)
     }
 
     default fn deposit(&mut self) -> Result<u128, ExchangeError> {
         let caller = Self::env().caller();
         let transfered_balance = Self::env().transferred_value();
+
         let native_reserve = Self::env()
             .balance()
             .checked_sub(transfered_balance)
             .ok_or(ExchangeError::SubUnderflow)?;
         let token_reserve = UsdTokenRef::balance_of(&self.get().usd_token, Self::env().account_id());
+
         let token_amount = transfered_balance
             .checked_mul(token_reserve)
             .ok_or(ExchangeError::MulOverflow)?
@@ -79,12 +86,14 @@ impl<T: ExchangeStorage> Exchange for T {
             .unwrap_or(0)
             .checked_add(liquidity_minted)
             .ok_or(ExchangeError::AddOverflow)?;
+
         self.get_mut().liquidity.insert(caller, &caller_liquidity);
         self.get_mut().total_liquidity = self
             .get()
             .total_liquidity
             .checked_add(liquidity_minted)
             .ok_or(ExchangeError::AddOverflow)?;
+
         UsdTokenRef::transfer_from_builder(
             &self.get().usd_token,
             caller,
@@ -95,12 +104,15 @@ impl<T: ExchangeStorage> Exchange for T {
         .call_flags(CallFlags::default().set_allow_reentry(true))
         .fire()
         .unwrap()?;
+
         Ok(liquidity_minted)
     }
 
     default fn withdraw(&mut self, amount: u128) -> Result<(u128, u128), ExchangeError> {
         let caller = Self::env().caller();
+
         let token_reserve = UsdTokenRef::balance_of(&self.get().usd_token, Self::env().account_id());
+
         let total_liquidity = self.get().total_liquidity;
         let native_amount = amount
             .checked_mul(Self::env().balance())
@@ -119,13 +131,16 @@ impl<T: ExchangeStorage> Exchange for T {
             .unwrap_or(0)
             .checked_sub(native_amount)
             .ok_or(ExchangeError::SubUnderflow)?;
+
         self.get_mut().liquidity.insert(caller, &caller_liquidity);
         self.get_mut().total_liquidity = total_liquidity
             .checked_sub(native_amount)
             .ok_or(ExchangeError::SubUnderflow)?;
+
         Self::env()
             .transfer(caller, token_amount)
             .map_err(|_| ExchangeError::NativeTransferFailed)?;
+
         Ok((native_amount, token_amount))
     }
 }
